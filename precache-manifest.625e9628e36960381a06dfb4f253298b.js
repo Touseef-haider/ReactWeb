ec873a5f0e56fa6a941e29dc2d8d1156e722d075
Touseef Haider <touseefhaider358@gmail.com>
2020-11-06 19:39:13 +0500

    Updates

---
+++ precache-manifest.625e9628e36960381a06dfb4f253298b.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8ccc66c9e253d6317f95e59d1a4524f",
+    "url": "/React/index.html"
+  },
+  {
+    "revision": "ce4b92752fcf820f0596",
+    "url": "/React/static/css/main.89601c70.chunk.css"
+  },
+  {
+    "revision": "1178acd767ceb82c0d5c",
+    "url": "/React/static/js/2.7645e1d4.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/React/static/js/2.7645e1d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce4b92752fcf820f0596",
+    "url": "/React/static/js/main.d8af9c21.chunk.js"
+  },
+  {
+    "revision": "79458da80355d5a677b7",
+    "url": "/React/static/js/runtime-main.1a456d4b.js"
+  },
+  {
+    "revision": "0bd2a5c8bb20f3e3e8a2d89a79539c38",
+    "url": "/React/static/media/Tanveer.0bd2a5c8.jpg"
+  },
+  {
+    "revision": "38e2ba90d00957e863c204ffc74d6f79",
+    "url": "/React/static/media/about.38e2ba90.jpg"
+  },
+  {
+    "revision": "79466b29cd2a908f27ce2c93a6779d2c",
+    "url": "/React/static/media/gfb.79466b29.jpg"
+  },
+  {
+    "revision": "def91b5015d15969082e97b202107fd9",
+    "url": "/React/static/media/haider.def91b50.jpg"
+  },
+  {
+    "revision": "34d59defe52931cdc2ea38192af55c0f",
+    "url": "/React/static/media/v-min.34d59def.png"
+  }
+]);
